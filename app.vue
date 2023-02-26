d48e7e49d85d7d175a5c18192c0c5d083c66299c
Dominik Dolejší <bukefalus007@gmail.com>
2023-02-26 13:31:36 +0100

    Repo Initialized

---
+++ app.vue
@@ -0,0 +1,7 @@
+<template>
+  <div>something</div>
+</template>
+
+<script lang="ts" setup>
+const str = ref("some string");
+</script>
---
+++ pages/neco.vue
@@ -0,0 +1,14 @@
+<script lang="ts" setup>
+definePageMeta({ middleware: ['auth'] })
+const count = ref(1)
+const double = computed(() => count.value * 2)
+const { data, refresh, pending } = await useAsyncData('/api/hello', () => $fetch('/api/hello'))
+</script>
+
+<template>
+  
+</template>
+
+<style scoped>
+  
+</style>
